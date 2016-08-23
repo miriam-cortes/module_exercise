5657c9ab3bd9320409648ae7f18b53192df92d0a
Miriam Cortes <mcortes.umiami@gmail.com>
2016-08-23 10:33:58 -0700

    class exercises from 8/23

--- module_exercise.rb
+++ module_exercise.rb
@@ -11,21 +11,21 @@
 ###################################
 
   #
-  # module Perimeter
+  # module Perimeter #a namespace for when your class name is crappy and you only want something *righthere* - should also be CamelCased
   #   class Array
   #     def initialize
   #       @size = 400
   #     end
   #   end
   # end
-  #
-  # our_array = Perimeter::Array.new
-  # ruby_array = Array.new
-  #
+  # #
+  # our_array = Perimeter::Array.new #how to call that Array in module Perimeter
+  # ruby_array = Array.new #regular array
+  # #
   # p our_array.class
   # p ruby_array.class
-  #
-  # p our_array.length
+  # #
+  # p our_array.length #this gives an error bc .length is a method of regular arrays and not our module Array
 
 
 
@@ -43,8 +43,7 @@
 # puts Faker::Hacker.adjective
 # puts Faker::Hacker.verb
 
-
-#Notice the format of how we're calling the functionality of the Faker gem, How is Faker's code structured?
+#Notice the format of how we're calling the functionality of the Faker gem, How is Faker's code structured? ** Inside modules
 
 
 
@@ -56,44 +55,49 @@
 
 #Testing out the traveller gem!
 
-  # require 'traveller'
-  #
-  # trav = Traveller.new("Seattle, Minnesota 98101")
-
-  # puts traveller1.city
-  # puts traveller1.zip
-  #
-  # trav.state = "Washington"
-  #
-  # puts traveller1.state
-
-
-###################################
-###### TRAVELLER GEM STEP TWO #####
-###################################
-
-#I want to create a new class for travellers, called traveller. What could possibly go wrong?
-  # class Traveller
-  #   attr_accessor :name, :email, :city, :state
-  #   def initialize(details_hash)
-  #     @name = details_hash[:name]
-  #     @email = details_hash[:email]
-  #     @location = Traveller.new(details_hash[:location])
-  #   end
-  # end
-
-  #Tests to use the Traveller Gem (after my class has been created. )
-    # traveller2 = Traveller.new("Chicago 60611 IL")
-    # puts Traveller.state
+#   require 'traveller'
+#
+#   traveller1 = Traveller.new("Seattle, Minnesota 98101")
+#   puts traveller1
+#   puts traveller1.city
+#   puts traveller1.zip
+#
+#   traveller1.state = "Washington"
+#   #
+#   puts traveller1.state
+#
+#
+# ###################################
+# ###### TRAVELLER GEM STEP TWO #####
+# ###################################
+#
+# #I want to create a new class for travellers, called traveller. What could possibly go wrong?
+# module Expedia
+#   class Traveller
+#     attr_accessor :name, :email, :location
+#     def initialize(details_hash)
+#       @name = details_hash[:name]
+#       @email = details_hash[:email]
+#       @location = details_hash[:location]
+#     end
+#   end
+# end
+#
+#   #Tests to use the Traveller Gem (after my class has been created. )
+#     traveller2 = Traveller.new("Chicago 60611 IL")
+#     puts traveller2.state
+#     location = Traveller.new("Seattle, Washington 98122")
+#
+# user1 = Expedia::Traveller.new(name:"name",email:"email",location:location)
 
   #Hmm. It doesn't work. Oh bother.
-  #How would we possibly fix this so I don't have to change the name of MY class but still be able to use this gem?
+  #How would we possibly fix this so I don't have to change the name of MY class but still be able to use this gem? ** put it in a module!
 
 
 
-  ###################################
-  #########     CONSTANTS    ########
-  ###################################
+###################################
+#########     CONSTANTS    ########
+###################################
 
   #A Ruby constant is like a variable, except that its value is supposed to remain constant for the duration of the program. The Ruby interpreter does not actually enforce the constancy of constants, but it does issue a warning if a program changes the value of a constant
 
@@ -104,44 +108,42 @@
   # Outside the class or module, they may be accessed using the scope operator, :: prefixed by an expression that returns the appropriate class or module.
 
   # Constants defined outside any class or module may be accessed as it is or by using the scope operator with no prefix.
-  # Constants may not be defined in methods.
+
+  # **Constants may not be defined in methods.**
 
   # Constants may be added to existing classes and modules from the outside by using the class or module name and the scope operator before the constant name.
 
 
 
-      #   module Blackjack
-      #     MAX_SCORE = 21
-      #     module Player
-      #         MAX_PLAYERS = 2
-      #         class Player
-      #           def initialize
-      #           end
-      #         end
-      #     end
-      #     module Card
-      #         MAX_VALUE = 13
-      #         class Card
-      #           def get_max_value()
-      #               return MAX_VALUE
-      #           end
-      #         end
-      #     end
-      # end
-      #
-      # MAX_SCORE = 50
-      # MAX_PLAYERS = 4
-      # MAX_VALUE = 21
-      #
-      # puts "MAX_SCORE - #{MAX_SCORE}"
-      # puts "Blackjack::MAX_SCORE - #{Blackjack::MAX_SCORE}"
-      #
-      # puts
-      #
-      # puts "MAX_Players - #{MAX_PLAYERS}"
-      # puts "Blackjack::Player::MAX_Players - #{Blackjack::Player::MAX_PLAYERS}"
-      #
-      # puts
-      #
-      # puts "MAX_VALUE - #{MAX_VALUE}"
-      # puts "Blackjack::Card::get_max_value -  #{Blackjack::Card::Card.new().get_max_value()}"`
+module Blackjack
+  MAX_SCORE = 21
+
+  module Player
+      MAX_PLAYERS = 2
+      class Player
+        def initialize
+        end
+      end
+  end
+  module Card
+      MAX_VALUE = 13
+      class Card
+        def get_max_value() #reader
+            return MAX_VALUE
+        end
+      end
+  end
+end
+
+  MAX_SCORE = 50
+  MAX_PLAYERS = 4
+  MAX_VALUE = 21
+
+  puts "MAX_SCORE - #{MAX_SCORE}" #this is the one from ln138
+  puts "Blackjack::MAX_SCORE - #{Blackjack::MAX_SCORE}" # ln119
+  #
+  puts "MAX_Players - #{MAX_PLAYERS}" # ln139
+  puts "Blackjack::Player::MAX_Players - #{Blackjack::Player::MAX_PLAYERS}" # ln122
+  #
+  puts "MAX_VALUE - #{MAX_VALUE}" # ln140
+  puts "Blackjack::Card::get_max_value -  #{Blackjack::Card::Card.new().get_max_value()}" # ln129
